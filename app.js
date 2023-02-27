79d257f5acaf6a67dfff803581db3d314c79e74d
Omar A. Orga <orgaomar8@gmail.com>
2023-02-27 17:40:43 +0800

    Add change grid size

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-const divContainer = document.querySelector(`.divContainer`);
-
+const boxContainer = document.querySelector(`.boxContainer`);
+const sizeRange = document.querySelector(`#sizeRange`);
+ 
 let drawing = false;
+// Initialize grid
+let gridSize = 16;
+createGrid(gridSize)
 
+sizeRange.addEventListener("input", (event) => {
+  // Get new grid size from range input value
+  gridSize = parseInt(event.target.value);
+  console.log(gridSize)
+  document.querySelector(`.grid-size`).textContent = `${gridSize} X ${gridSize} `;
+  // Remove existing grid items
+  boxContainer.innerHTML = "";
+  // Create new grid with updated size
+  createGrid(gridSize);
+});
 
-for (let i = 0; i < 256; i++) {
+function createGrid(gridSize){
+
+  boxContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  boxContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+for (let i = 0; i < (gridSize*gridSize); i++) {
   const div = document.createElement(`div`);
   div.classList.add(`box`);
   div.addEventListener(`mousedown`, paint);
@@ -13,7 +32,8 @@ for (let i = 0; i < 256; i++) {
   div.addEventListener(`dragstart`, (event) => {
     event.preventDefault();
   });
-  divContainer.appendChild(div);
+  boxContainer.appendChild(div);
+}
 }
 
 function paint(e) {
